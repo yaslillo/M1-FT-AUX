6a4cf13f7dbda34a56b9700653b8ba0e471ebc94
Daniel <daniel.maers@gmail.com>
2022-02-24 14:23:54 -0300

    update
    update

--- 04-Stack/04-stack.js
+++ 04-Stack/04-stack.js
@@ -1,4 +1,4 @@
-const { Stack } = require("../estructuras")
+const { Stack, Queue } = require("../estructuras")
 
 // Utilizando un STACK, y dado un string, invertir el orden de las palabras.
 // OJO: Para este ejercicio NO se pueden usar metodos de Array.
@@ -16,12 +16,35 @@ const { Stack } = require("../estructuras")
 
 
 function efectoEspejo(str){  
+//tu codigo aqui
 
     
 };
 
 
 
+function turnos(queue){
+// Un hospital necesita necesita aplicar un sistema de turnos. Para ello se implementara una Queue 
+// en la cual iran ingresando los datos de cada paciente.
+
+// paciente:{
+//     nombre: "Daniel",
+//     apellido: "Maers",
+//     edad: 32,
+//     estado: "en espera"
+// }
+
+// en base a esto, implementar las siguientes funciones en el prototipo de Queue:
+// * siguienteTurno: deberá buscar si algun paciente posee el estado "prioridad", en caso positivo
+// quitar a ese paciente de la Queue, pasar su estado a "atendido", y retornarlo. en caso contrario
+// quitar el primer elemento de la Queue de la misma forma antes mencionada.
+//UTILIZAR UNA STACK PARA RECORRER LA QUEUE Y MANTENER EL ORDEN 
+
+//TU CODIGO AQUI
+
+
+
+}
 
 
 
@@ -29,4 +52,5 @@ function efectoEspejo(str){
 
 module.exports = {
     efectoEspejo,
+    turnos
 }
