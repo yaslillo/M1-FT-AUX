457713d52a40a5f8791a02bbd9db1b53526d38b4
Jime227 <jamedina@live.com.ar>
2022-02-25 15:33:37 -0300

    recursion

---
+++ 06-Recursion/recursion.js
@@ -0,0 +1,27 @@
+// Usando la recursion calcular el producto de todos los numeros de dado arreglo
+// ej:
+// producto([1, 2, 5]) devuelve 10
+// producto([7, 2, 5]) devuelve 70
+const producto = function (array) {
+  //escribe aqui tu codigo
+};
+
+// Dado un objeto con objetos anidados utilizar la recursión para crear una función
+// que devuelva true o false dependiendo si el objeto tiene o no el valor pasado por parametro
+// ejemplo:
+// let obj = {
+//     school: {
+//         hogwarts: {
+//             headmaster:{
+//               name: {
+//                 first: "Albus",
+//                 last: "Dumbledore"
+//               }
+//             }
+//         }
+//     }
+// }
+const isThere = function (obj, value) {
+  //escribe aqui tu codigo
+};
+module.exports = { producto, isThere };
