1ac9fe9b37d44cc95acad028d7ad68248f649129
JJSolari <jignaciosolari@gmail.com>
2022-02-25 09:51:10 -0300

    arrays finished

--- 01-arrays/01-arrays.js
+++ 01-arrays/01-arrays.js
@@ -2,14 +2,34 @@
 function invertirOrden(array) {
     // Invertir el orden de los elementos del array que recibe por parametro
     // Que los ultimos elementos, pasen a ser los primeros
+    //
+    // DETALLE: En caso de que el elemento contenga mas de 1 digito, el mismo NO debera ser devuelto
     // No vale usar el metodo "reverse"
+ 
+};
+
+
+function numeroEnComun(array1, array2) {
+    // Entre los dos array's que recibe la funcion por parametro
+    // Buscar y retornar el valor en comun entre ellos
+
+};
+
+
+
+function sumaDeArrays(array) {
+    // El array recibido por parametro es un array multidimencional con array's que contienen elementos de tipo number
+    // Tienen que devolver UN SOLO array que solo contenga elementos de tipo number
+    // Sumando los elementos de cada array que contenga dos elementos, y devolviendo la suma del mismo
+    // OJO: Si el elemento dentro del array que ingresa por prop, ya es de tipo number, deben devolverlo como tal dentro del array que retornan.
     
 };
 
 
-function elementoEnComun(array1, array2) {
-    // Entre los dos array's que recibe la funcion por parametro
-    // Buscar y retornar el valor en comun entre ellos
+function mismoValorMismosElementos(numero, divisor) {
+    // Tiene que devolver un array con la misma cantidad de elementos que el valor del divisor
+    // Todos los elementos deben tener el mismo valor
+    // OJO: Si el resultado de la division no es un entero, deben devolver false
     
 };
 
@@ -18,40 +38,15 @@ function elementoMenorYMayor(array) {
     // El Array recibido por props es un array que contienen numeros
     // Tenes que retornar un array
     // Solamente con el elemento menor y mayor del array recibido
-  
+   
 };
 
-function sumaDeArrays(array) {
-    // El array recibido por parametro es un array multidimencional con array's que contienen elementos de tipo number
-    // Tienen que devolver un array que solo contenga elementos de tipo number
-    // Sumando los elementos de cada array, y devolviendo la suma del mismo
-    // OJO: Si el elemento dentro del array que ingresa por prop, ya es de tipo number, deben devolverlo como tal dentro del array que retornan.
-
-};
-
-
-function cazaDeNumeros(array) {
-   // El array recibido por parametro contiene un Objeto
-   // Tienen que devolver en un array, todos los valores numericos que se encuentren en el objeto
-   // Pista: podes utilizar el operador typeof.
-   // Pista 2: Los valores del objeto recibido, solo seran de tipo array y de tipo number
-
-
-};
-
-function mismoValorMismosElementos(numero, divisor) {
-    // Tiene que devolver un array con la misma cantidad de elementos que el valor del divisor
-    // Todos los elementos deben tener el mismo valor
-
-
-}
 
 
 module.exports = {
-    elementoEnComun,
+    numeroEnComun,
     invertirOrden,
     elementoMenorYMayor,
     sumaDeArrays,
-    cazaDeNumeros,
     mismoValorMismosElementos
 };
